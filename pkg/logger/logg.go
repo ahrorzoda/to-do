070bbbb0d79ac70ce249e76f1f19cb263be4af00
shohrukhahrorzoda <Shakhrukhjane@gmail.com>
2023-10-04 15:27:20 +0500

    add a logging file

--- pkg/logger/logg.go
+++ pkg/logger/logg.go
@@ -1,1 +1,31 @@
-package logger
+package loggers
+
+import (
+	"log"
+	"os"
+)
+
+// Инициализируем логгеры для трех файлов
+var (
+	Info  *log.Logger
+	Error *log.Logger
+)
+
+func init() {
+	// Создаем файлы логирования (если они не существуют)
+	if _, err := os.Stat("../pkg/logger/file/info.log"); os.IsNotExist(err) {
+		os.Create("info.log")
+	}
+
+	if _, err := os.Stat("../pkg/logger/file/error.log"); os.IsNotExist(err) {
+		os.Create("error.log")
+	}
+
+	// Открываем файлы логирования для записи
+	infoFile, _ := os.OpenFile("../pkg/logger/file/info.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	errorFile, _ := os.OpenFile("../pkg/logger/file/error.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	// Устанавливаем вывод логов в соответствующие файлы
+	Info = log.New(infoFile, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(errorFile, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
