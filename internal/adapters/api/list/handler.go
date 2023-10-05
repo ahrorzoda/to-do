0254e0e4934edbf2767760707cef049edd5a5229
shohrukhahrorzoda <Shakhrukhjane@gmail.com>
2023-10-05 09:14:37 +0500

    try writing a new route

--- internal/adapters/api/list/handler.go
+++ internal/adapters/api/list/handler.go
@@ -1,1 +1,19 @@
 package list
+
+import (
+	"github.com/ahrorzoda/to-do/internal/adapters/api"
+	"github.com/gin-gonic/gin"
+)
+
+type handler struct {
+	listService Service
+}
+
+func NewHandler(service Service) api.Handler {
+	return &handler{listService: service}
+}
+
+func (h *handler) Register(rout *gin.Engine) {
+	rout.GET("/list/get/all", h.listService.GetListByUUID)
+	panic("implement me")
+}
