7d2db8ed856afe8d2571ab44cee230b715a7d7f7
shohrukhahrorzoda <Shakhrukhjane@gmail.com>
2023-10-05 09:21:58 +0500

    implementation of two methods GetListByUUID(), GetLimOffList() and add field 'db' for listStorage

--- internal/adapters/db/list/storage.go
+++ internal/adapters/db/list/storage.go
@@ -1,23 +1,37 @@
 package list
 
-import "github.com/ahrorzoda/to-do/internal/domain/list"
+import (
+	"github.com/ahrorzoda/to-do/internal/domain/list"
+	"gorm.io/gorm"
+)
 
 type listStorage struct {
+	db *gorm.DB
 }
 
-func NewStorage() list.List {
-	return &listStorage{}
+func NewStorage(db *gorm.DB) list.Storage {
+	return &listStorage{db: db}
 }
 
-func (ls *listStorage) GetOneList(uuid string) *list.List {
+func (ls *listStorage) GetListByUUID(uuid string) *list.List {
+	//TODO implement me
+	panic("implement me")
 	return nil
 }
 
 func (ls *listStorage) GetLimOffList(limit, offset int) []*list.List {
-
+	return nil
 }
 
-func (ls *listStorage) CreateList(list *list.List) *list.List {
+func (ls *listStorage) CreateList(list *list.CreateList) *list.List {
+	//TODO implement me
+	panic("implement me")
+	return nil
+}
+
+func (ls *listStorage) UpdateList(uuid string) *list.List {
+	//TODO implement me
+	panic("implement me")
 	return nil
 }
 
