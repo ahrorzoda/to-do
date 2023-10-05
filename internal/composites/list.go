eab9bd7f8f92a879770808b89f393b4077dfa387
shohrukhahrorzoda <Shakhrukhjane@gmail.com>
2023-10-05 09:30:46 +0500

    add a new parameter for NewListComposite function

--- internal/composites/list.go
+++ internal/composites/list.go
@@ -1,1 +1,25 @@
 package composites
+
+import (
+	"github.com/ahrorzoda/to-do/internal/adapters/api"
+	list2 "github.com/ahrorzoda/to-do/internal/adapters/api/list"
+	list3 "github.com/ahrorzoda/to-do/internal/adapters/db/list"
+	"github.com/ahrorzoda/to-do/internal/domain/list"
+)
+
+type ListComposite struct {
+	Storage list.Storage
+	Service list2.Service
+	Handler api.Handler
+}
+
+func NewListComposite(postgresComposite *PostgresDBComposite) (*ListComposite, error) {
+	storage := list3.NewStorage(postgresComposite.db)
+	service := list.NewService(storage)
+	handler := list2.NewHandler(service)
+	return &ListComposite{
+		Storage: storage,
+		Service: service,
+		Handler: handler,
+	}, nil
+}
