b6c724195b88550c41f979137c1bfa07292238c3
shohrukhahrorzoda <Shakhrukhjane@gmail.com>
2023-10-09 19:54:27 +0500

    continuation implementation function adding new tasks

--- internal/adapters/db/list/storage.go
+++ internal/adapters/db/list/storage.go
@@ -1,40 +1,49 @@
-package list
+package List
 
 import (
 	"github.com/ahrorzoda/to-do/internal/domain/list"
+	logger "github.com/ahrorzoda/to-do/pkg/logger"
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"net/http"
 )
 
-type listStorage struct {
+type ListStorage struct {
 	db *gorm.DB
 }
 
 func NewStorage(db *gorm.DB) list.Storage {
-	return &listStorage{db: db}
+	return &ListStorage{db: db}
 }
 
-func (ls *listStorage) GetListByUUID(uuid string) *list.List {
+func (ls *ListStorage) GetListByUUIDStorage(uuid string) *list.Task {
+	return nil
+}
+
+func (ls *ListStorage) GetLimOffList(limit, offset int) []*list.Task {
+	return nil
+}
+
+//NOTE: добавление новых задач
+
+func (ls *ListStorage) CreateList(ctx *gin.Context, lst *list.Task) *list.Task {
+	err := ls.db.Create(&lst).Error
+	if err != nil {
+		logger.Error.Println("CreateList(): Error in create list ==> %s", err.Error())
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return nil
+	}
+	return lst
+}
+
+func (ls *ListStorage) UpdateList(uuid string) *list.Task {
 	//TODO implement me
 	panic("implement me")
 	return nil
 }
 
-func (ls *listStorage) GetLimOffList(limit, offset int) []*list.List {
-	return nil
-}
-
-func (ls *listStorage) CreateList(list *list.CreateList) *list.List {
-	//TODO implement me
-	panic("implement me")
-	return nil
-}
-
-func (ls *listStorage) UpdateList(uuid string) *list.List {
-	//TODO implement me
-	panic("implement me")
-	return nil
-}
-
-func (ls *listStorage) DeleteList(uuid string) error {
+func (ls *ListStorage) DeleteList(uuid string) error {
 	return nil
 }
